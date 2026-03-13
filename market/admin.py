8d7c8558910d5c7cc151088de415a5b3f2384b79
tessbyrne <3117688B@student.gla.ac.uk>
2026-03-13 18:39:18 +0000

    Registering market models

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Item, ItemPhoto, Transaction
 
-# Register your models here.
+class ItemPhotoInline(admin.TabularInline):
+    model = ItemPhoto
+    extra = 1
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "seller","category", "price", "status", "listed_at")
+    list_filter = ("category", "status")
+    search_fields = ("title", "description")
+    inlines = [ItemPhotoInline]
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("type", "buyer", "item", "amount", "date")
+    list_filter = ("type", )
+    search_fields = ("buyer__email", "item__title")
+
