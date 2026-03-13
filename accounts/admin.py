f0062bca1857ad6d1dad822e9efec1f5b62ef4d2
tessbyrne <3117688B@student.gla.ac.uk>
2026-03-13 15:00:26 +0000

    Register user model with admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    list_display = ("email", "first_name", "last_name", "student_id", "account_balance", "is_staff")
+    ordering = ("email", )
+    search_fields = ("email", "student_id", "first_name", "last_name")
+    
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "student_id", "profile_photo_url", "account_balance")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    
+    add_fieldsets = (
+        (None, {"classes": ("wide",),
+                "fields": ("email", "first_name", "last_name", "student_id", "password1", "password2")}),
+    )
+
