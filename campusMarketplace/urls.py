a2ca0823d024d530f02980f69f53ddf6767b437a
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-03-15 01:58:49 +0000

    Initialised Basic Views - Ore
    Created and associated each URL without a slug with a simple HTTP response

--- campusMarketplace/urls.py
+++ campusMarketplace/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('', views.shop, name='shop'),
     # path('/category/<slug:category_name_slug>/', views.category, name='category'),
     # path('/item/<slug:item_name_slug>/', views.item, name='item'),
-    path('new_item/', views.new_item, name='new_item'),
+    path('new-item/', views.createListing, name='newItem'),
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('account/', views.account, name='account'),
     ]
 
--- campusMarketplace/views.py
+++ campusMarketplace/views.py
@@ -4,9 +4,17 @@ from django.http import HttpResponse
 def shop(request):
      return HttpResponse("This is the main page.")
 
+def register(request):
+     return HttpResponse("This is the registration page.")
+
+def login(request):
+     return HttpResponse("This is the login page.")
+
+def logout(request):
+     return HttpResponse("This is the logout page.")
+
 def newItem(request):
      return HttpResponse("This is the new item page.")
 
 def account(request):
-     return HttpResponse("This is the account page.")
-
+     return HttpResponse("This is the user account page.")
