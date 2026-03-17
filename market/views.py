53384b6604d70b6b0c2ac18ac661a5e80eb95bdf
tessbyrne <3117688B@student.gla.ac.uk>
2026-03-17 14:28:06 +0000

    debugging existing accounts

--- market/views.py
+++ market/views.py
@@ -36,7 +36,7 @@ def register(request):
             user_form = UserForm()
             profile_form = UserProfileForm()
 
-     return render(request, 'accounts/register.html',context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+     return render(request, 'market/register.html',context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
               
           
 
